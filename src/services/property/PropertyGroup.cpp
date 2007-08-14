dc5d1c294ff3f80c29b97ed2c55c5ca153a6d64b
Filip Volejnik <f.volejnik@centrum.cz>
2007-08-14 12:23:25 +0000

    Adding inheritor into the prop. service makes it a bit more complete

--- src/services/property/PropertyGroup.cpp
+++ src/services/property/PropertyGroup.cpp
@@ -20,233 +20,240 @@
  *****************************************************************************/
 
 #include "PropertyGroup.h"
+#include "InheritService.h"
 
 using namespace std;
 
 namespace Opde {
-	
+
 	// --------------------------------------------------------------------------
-	PropertyGroup::PropertyGroup(const std::string& name, const std::string& chunk_name, DTypeDefPtr type, uint ver_maj, uint ver_min) :
-			mPropertyListeners(), 
-			mName(name), 
+	PropertyGroup::PropertyGroup(const std::string& name, const std::string& chunk_name, DTypeDefPtr type, uint ver_maj, uint ver_min, string inheritorName) :
+			mPropertyListeners(),
+			mName(name),
 			mChunkName(chunk_name),
 			mType(type),
 			mVerMaj(ver_maj),
 			mVerMin(ver_min) {
-		
+
+		// Find the inheritor by the name, and assign too
+		InheritServicePtr inhs = ServiceManager::getSingleton().getService("InheritService").as<InheritService>();
+		mInheritor = inhs->createInheritor(inheritorName);
 	}
-	
+
 	// --------------------------------------------------------------------------
 	PropertyGroup::~PropertyGroup() {
 		clear();
 		mPropertyListeners.clear(); // those which did not ask for removal
-		
-		
+
+
 	}
-	
+
 	// --------------------------------------------------------------------------
 	PropertyDataPtr PropertyGroup::getData(int obj_id) {
 		PropertyStore::const_iterator it = mPropertyStore.find(obj_id);
-		
+
 		if (it != mPropertyStore.end())
 			return it->second;
 		else
 			LOG_ERROR("PropertyGroup::getData : Property for object ID %d was not found in group %s", obj_id, mName.c_str());
 	}
-	
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::load(FileGroup* db) {
 		// Open the chunk specified by "P$" + mChunkName
 		FilePtr fprop;
-		
+
 		string pchn = "P$" + mChunkName;
-		
+
 		try {
 			fprop = db->getFile(pchn);
 		} catch (BasicException& e) {
 			LOG_FATAL("PropertyGroup::load : Could not find the property chunk %s", pchn.c_str());
 			return;
 		}
-		
+
 		// Can't calculate the count of the properties, as they can have any size
 		// load. Each record has: OID, size (32 bit uint's)
 		int id = 0xDEADBABE;
 		uint32_t size;
-		
+
 		while (!fprop->eof()) {
 			// load the id
 			fprop->readElem(&id, sizeof(uint32_t));
 			// Load the size
 			fprop->readElem(&size, sizeof(uint32_t));
-			
+
 			if (id == 0)
 				LOG_ERROR("PropertyGroup: P$%s : Obj ID == 0", mChunkName.c_str());
-			
+
 			LOG_DEBUG("PropertyGroup: P$%s : Loading property %s for obj %d (Sizes: real %d, type %d)", mChunkName.c_str(), mName.c_str(), id, size, mType->size());
 			// create the property
 			PropertyDataPtr prop = new PropertyData(id, mType, fprop, size);
-			
+
 			if (!_addProperty(prop))
 				LOG_ERROR("PropertyGroup: P$%s : Cannot add property for obj %d (already there)", mChunkName.c_str(), id);
 		}
 	}
-	
-	
+
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::save(FileGroup* db, uint saveMask) {
 		// Open the chunk specified by "P$" + mChunkName
 		FilePtr fprop;
-		
+
 		string pchn = "P$" + mChunkName;
-		
+
 		try {
 			fprop = db->createFile(pchn, mVerMaj, mVerMin);
 		} catch (BasicException& e) {
 			LOG_FATAL("PropertyGroup::save : Could not create property chunk %s", pchn.c_str());
 			return;
 		}
-		
+
 		// Can't calculate the count of the properties, as they can have any size
 		// load. Each record has: OID, size (32 bit uint's)
 		int id;
 		uint32_t size;
-		
+
 		PropertyStore::const_iterator it = mPropertyStore.begin();
-		
+
 		for (; it != mPropertyStore.end() ; ++it) {
-			
+
 			id = it->second->id();
-			
+
 			// Determine if the prop should be included, based on it's mask
 			uint objmask = 0;
-			
+
 			if (id < 0)
 				objmask = 1;
-			
+
 			if (id > 0)
 				objmask = 2;
-			
+
 			if (!(saveMask & objmask))
 				continue;
-			
+
 			size= it->second->size();
 			// load the id
 			fprop->writeElem(&id, sizeof(uint32_t));
 			// Load the size
 			fprop->writeElem(&size, sizeof(uint32_t));
-			
+
 			LOG_DEBUG("PropertyGroup: P$%s : Writing property %s for obj %d", mChunkName.c_str(), mName.c_str(), id, size, mType->size());
-			
+
 			it->second->serialize(fprop);
-			
+
 		}
 	}
-	
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::clear() {
 		PropertyChangeMsg msg;
-		
+
 		msg.change = PROP_GROUP_CLEARED;
 		msg.objectID = 0;
 		msg.data = PropertyDataPtr(); // NULL that means
-		
+
 		broadcastPropertyMessage(msg);
-		
+
 		mPropertyStore.clear();
+		mInheritor->clear();
 	}
-	
+
 	// --------------------------------------------------------------------------
 	bool PropertyGroup::createProperty(int obj_id) {
 		PropertyDataPtr propd = new PropertyData(obj_id, mType);
-		
+
 		return _addProperty(propd);
 	}
-	
+
 	// --------------------------------------------------------------------------
 	bool PropertyGroup::createProperty(int obj_id, DTypePtr data) {
 		// simply compare the type pointers...
 		if (data->type() != mType)
 			OPDE_EXCEPT("Incompatible types when creating link data", "PropertyGroup::createProperty");
-		
+
 		PropertyDataPtr propd = new PropertyData(obj_id, data);
-		
+
 		return _addProperty(propd);
 	}
-		
+
 	// --------------------------------------------------------------------------
 	bool PropertyGroup::removeProperty(int obj_id) {
 		size_t erased = mPropertyStore.erase(obj_id);
-		
+        mInheritor->setImplements(obj_id, false);
+
 		if (erased) {
 			// TODO: Notify inheritor!
-			
+
 			PropertyChangeMsg msg;
-			
+
 			msg.change = PROP_REMOVED;
 			msg.objectID = obj_id;
 			msg.data = PropertyDataPtr(); // NULL that means
-			
+
 			broadcastPropertyMessage(msg);
-			
+
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	// --------------------------------------------------------------------------
 	bool PropertyGroup::cloneProperty(int obj_id, int src_id) {
 		PropertyDataPtr pd = getData(src_id);
-		
+
 		if (!pd.isNull()) {
 			/*PropertyDataPtr propd = new PropertyData(obj_id, *pd);
-			
+
 			return _addProperty(propd);*/
 		}
 	}
-	
-	
+
+
 	// --------------------------------------------------------------------------
 	bool PropertyGroup::_addProperty(PropertyDataPtr propd) {
 		pair<PropertyStore::iterator, bool> res = mPropertyStore.insert(make_pair(propd->id(), propd));
-		
+        mInheritor->setImplements(propd->id(), true);
+
 		if (res.second) {
 			// TODO: Notify inheritor!
-			
+
 			PropertyChangeMsg msg;
-			
+
 			msg.change = PROP_ADDED;
 			msg.objectID = propd->id();
 			msg.data = propd;
-			
+
 			broadcastPropertyMessage(msg);
-			
+
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::broadcastPropertyMessage(const PropertyChangeMsg& msg) const {
 		PropertyListeners::iterator it = mPropertyListeners.begin();
-		
+
 		for (; it != mPropertyListeners.end(); ++it) {
-			// Call the method on the listener pointer 
+			// Call the method on the listener pointer
 			((*it)->listener->*(*it)->method)(msg);
 		}
 	}
-	
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::registerListener(PropertyChangeListenerPtr* listener) {
 		mPropertyListeners.insert(listener);
 	}
-	
+
 	// --------------------------------------------------------------------------
 	void PropertyGroup::unregisterListener(PropertyChangeListenerPtr* listener) {
 		mPropertyListeners.erase(listener);
 	}
 
-	
+
 }
- 
+
--- src/services/property/PropertyGroup.h
+++ src/services/property/PropertyGroup.h
@@ -19,7 +19,7 @@
  *
  *****************************************************************************/
 
- 
+
 #ifndef __PROPERTYGROUP_H
 #define __PROPERTYGROUP_H
 
@@ -29,9 +29,10 @@
 #include "FileGroup.h"
 #include "OpdeException.h"
 #include "logger.h"
+#include "InheritService.h"
 
 namespace Opde {
-	
+
 	/** @brief Property group - a group of properties of the same kind (name, type).
 	* Property group holds all the properties of the same kind for all the objects.
 	*/
@@ -44,94 +45,94 @@ namespace Opde {
 			* @param ver_maj The major version of the chunk that stores this property
 			* @param ver_min The minor version of the chunk that stores this property
 			*/
-			PropertyGroup(const std::string& name, const std::string& chunk_name, DTypeDefPtr type, uint ver_maj, uint ver_min);
-			
+			PropertyGroup(const std::string& name, const std::string& chunk_name, DTypeDefPtr type, uint ver_maj, uint ver_min, std::string inheritorName);
+
 			/** Destructor */
 			virtual ~PropertyGroup();
-			
+
 			/** Property data getter. Used for more massive property changes.
-			* @param obj_id Object ID of the property to fetch 
-			* @note call dataChangeFinished(obj_id), otherwise listeners will not receive broadcasts about the data modification 
-			* @return PropertyDataPtr, which will be null if the property was not found 
+			* @param obj_id Object ID of the property to fetch
+			* @note call dataChangeFinished(obj_id), otherwise listeners will not receive broadcasts about the data modification
+			* @return PropertyDataPtr, which will be null if the property was not found
 			*/
 			PropertyDataPtr getData(int obj_id);
-			
-			/** Determines whether an object with id obj_id stores or inherits property of this type 
-			* @param obj_id The object id of the object 
+
+			/** Determines whether an object with id obj_id stores or inherits property of this type
+			* @param obj_id The object id of the object
 			* @return true if the object has the given property
 			 */
 			bool has(int obj_id) const {
 				// Get the effective id for the object
 				int eoid = _getEffectiveObject(obj_id);
-				
+
 				if (eoid == 0)
 					return false;
-				
+
 				// This code could be removed, if we knew the nonzero eoid will always mean prop exists
 				return owns(eoid);
 			}
-			
-			/** Determines whether an object with id obj_id stores property of this type 
-			* @param obj_id The object id of the object 
+
+			/** Determines whether an object with id obj_id stores property of this type
+			* @param obj_id The object id of the object
 			* @return true if the object has the given property
 			* @note Will return false if the object only inherits the property, but does not own it (use has for that)
 			*/
 			bool owns(int obj_id) const {
 				PropertyStore::const_iterator it = mPropertyStore.find(obj_id);
-				
+
 				if (it != mPropertyStore.end())
 					return true;
 				else
 					return false;
 			}
-			
+
 			/** Loads properties from a file group
 			* @param db The database to load from */
 			void load(FileGroup* db);
-			
+
 			/** Saves properties to a file group
 			* @param db The database to save to
 			* @param saveMask The mask to use while saving (1 - archetypes, 2 - instances, 3 - both)
 			*/
 			void save(FileGroup* db, uint saveMask);
-			
+
 			/** Clears the whole property group.
 			* Clears out all the properties, and broadcasts PROP_GROUP_CLEARED
 			*/
 			void clear();
-			
+
 			/** Creates a property for given object ID, using the default values for the property fields
-			* @param obj_id The id of the object to create the property for 
+			* @param obj_id The id of the object to create the property for
 			* @return true if the property was created, false if the object ID already holds the given property
-			* @note Broadcasts a PROP_ADDED on success 
+			* @note Broadcasts a PROP_ADDED on success
 			* @note Notifies inheritor about the change */
 			bool createProperty(int obj_id);
-			
+
 			/** Creates a property for given object ID, using specified values for the property fields
-			* @param obj_id The id of the object to create the property for 
+			* @param obj_id The id of the object to create the property for
 			* @param data The property data to use ()
 			* @return true if the property was created, false if the object ID already holds the given property
-			* @note Broadcasts a PROP_ADDED on success 
+			* @note Broadcasts a PROP_ADDED on success
 			* @note Notifies inheritor about the change */
 			bool createProperty(int obj_id, DTypePtr data);
-			
+
 			/** Creates a property for given object ID
-			* @param obj_id The id of the object to create the property for 
+			* @param obj_id The id of the object to create the property for
 			* @return true if the property was removed, false if the object ID didn't hold the property
-			* @note Broadcasts a PROP_REMOVED on success 
+			* @note Broadcasts a PROP_REMOVED on success
 			* @note Notifies inheritor about the change */
 			bool removeProperty(int obj_id);
-			
-			/** Creates a new property by cloning a given property on object ID 
-			* @param obj_id Target object ID (id to create) 
+
+			/** Creates a new property by cloning a given property on object ID
+			* @param obj_id Target object ID (id to create)
 			* @param src_id The id of the object to clone property from */
 			bool cloneProperty(int obj_id, int src_id);
-			
+
 			// ----------------- Data manipulation releted methods --------------------
 			/** Manual broadcaster of data change. To be used as the last call in a block of property manipulation calls using the PropertyDataPtr from getData call.
 			* @see getData
 			* @param data The new data (will not replace the old, used for objectID and data fields in the broadcast)
-			* 
+			*
 			*/
 			void dataChangeFinished(PropertyDataPtr data) const {
 				// TODO: Create a broadcast message, and send
@@ -139,112 +140,114 @@ namespace Opde {
 				msg.change = PROP_CHANGED;
 				msg.objectID = data->id();
 				msg.data = data;
-				
+
 				broadcastPropertyMessage(msg);
 			}
-			
+
 			/** Direct data setter.
 			* @param id object id
-			* @param field the field name 
-			* @param value New value for the field 
+			* @param field the field name
+			* @param value New value for the field
 			* @return true if the change was sucessful
 			* @see owns
 			* @note Will log error when object id does not own the property to be changed */
 			bool set(int id, std::string field, const DVariant& value) {
 				PropertyStore::const_iterator it = mPropertyStore.find(id);
-				
+
 				if (it != mPropertyStore.end()) {
-					it->second->set(field, value);	
+					it->second->set(field, value);
 					return true;
 				}
-				
+
 				LOG_ERROR("PropertyGroup::set : Property for object ID %d was not found in group %s", id, mName.c_str());
 				return false;
 			}
-			
+
 			/** Direct data getter
 			* @param id object id
-			* @param field the field name 
+			* @param field the field name
 			* @return The value from the field indicated, or empty DVariant if the object id does not own the property
 			* @see owns
-			* @note Will silently fail when id is non-valid. Check error log for this happening 
+			* @note Will silently fail when id is non-valid. Check error log for this happening
 			*/
 			DVariant get(int id, std::string field) {
 				PropertyStore::const_iterator it = mPropertyStore.find(id);
-				
+
 				if (it != mPropertyStore.end()) {
 					return it->second->get(field);
 				}
-				
+
 				LOG_ERROR("PropertyGroup::get : Property for object ID %d was not found in group %s", id, mName.c_str());
 				return DVariant();
 			}
-			
-			
+
+
 			// ----------------- Listener releted methods --------------------
 			/** Registers a property change listener.
 			* @param listener A pointer to PropertyChangeListenerPtr
-			* @note The same pointer has to be supplied to the unregisterListener in order to succeed with unregistration  
+			* @note The same pointer has to be supplied to the unregisterListener in order to succeed with unregistration
 			*/
 			void registerListener(PropertyChangeListenerPtr* listener);
-			
-			
+
+
 			/** Unregisters a property change listener
 			* @param listener A pointer to PropertyChangeListenerPtr
-			* @note The same pointer has to be supplied to the unregisterListener in order to succeed with unregistration  
+			* @note The same pointer has to be supplied to the unregisterListener in order to succeed with unregistration
 			*/
 			void unregisterListener(PropertyChangeListenerPtr* listener);
-			
+
 		protected:
 			/** Inserts the property into the group, notifies inheritors and broadcasts the change
 			* @param propd The property to add
 			*/
 			bool _addProperty(PropertyDataPtr propd);
-			
+
 			/// Send a message to all listeners of relation change
 			void broadcastPropertyMessage(const PropertyChangeMsg& msg) const;
 
-			
+
 			/** Returns an ID of the object which is responsible for the current property values
-			* As the properties can be inherited using both archetype links and MetaProps, 
-			* there must be a way to know what object currently represents the property values 
+			* As the properties can be inherited using both archetype links and MetaProps,
+			* there must be a way to know what object currently represents the property values
 			* @param obj_id The object id to query
 			* @return object ID of the effective property holder, or zero (0) if not found */
 			int _getEffectiveObject(int obj_id)  const {
-				// TODO: Ask inheritor for the value once it is implemented
-				return obj_id;
+				return mInheritor->getEffectiveID(obj_id);
 			}
-			
+
 			/// Stores objectID -> Property
 			typedef std::map< int, PropertyDataPtr > PropertyStore;
-			
+
 			/// Listener set
 			typedef std::set< PropertyChangeListenerPtr* > PropertyListeners;
-			
+
 			/// Set of listeners that receive property events
 			PropertyListeners mPropertyListeners;
-			
+
 			/// Property store instance
 			PropertyStore mPropertyStore;
-			
+
 			/// The name of the property
 			std::string mName;
-			
+
 			/// The name of the chunk data (reduced of the P$)
 			std::string mChunkName;
-			
+
 			/// chunk version - major
 			uint mVerMaj;
 			/// chunk version - minor
 			uint mVerMin;
-			
+
 			/// Type definition for the property data
 			DTypeDefPtr mType;
+
+            /// Inheritor used to determine property inheritance
+			InheritorPtr mInheritor;
 	};
-	
+
 	/// Shared pointer to property group
 	typedef shared_ptr<PropertyGroup> PropertyGroupPtr;
-	
+
 }
- 
+
 #endif
