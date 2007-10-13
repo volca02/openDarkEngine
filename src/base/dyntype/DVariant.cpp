2827d53d059d3f1df2b707074fd8878727137bd9
Filip Volejnik <f.volejnik@centrum.cz>
2007-10-13 14:24:56 +0000

    random small corrections

--- src/base/dyntype/DVariant.cpp
+++ src/base/dyntype/DVariant.cpp
@@ -18,39 +18,39 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *****************************************************************************/
- 
+
 #include <stdexcept>
 #include "DVariant.h"
 
 using namespace std;
 
-namespace Opde {	
+namespace Opde {
 	/*------------------------------------------------------*/
 	/*-------------------- DVariant ------------------------*/
 	/*------------------------------------------------------*/
-	
+
 	DVariant::DVariant() {
 		mPrivate.isShared = false;
 		mPrivate.type = DV_INVALID;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(Type t, void *val) {
 		mPrivate.isShared = false;
-		
+
 		switch (t) {
 			case DV_BOOL :
 				mPrivate.data.dbool = val ? *static_cast<bool *>(val) : false;
 				break;
-				
+
 			case DV_FLOAT :
 				mPrivate.data.dfloat = val ? *static_cast<float *>(val) : 0;
 				break;
-				
+
 			case DV_INT :
 				mPrivate.data.dint = val ? *static_cast<int *>(val) : 0;
 				break;
-				
+
 			case DV_UINT :
 				mPrivate.data.duint = val ? *static_cast<uint *>(val) : 0;
 				break;
@@ -59,37 +59,37 @@ namespace Opde {
 				mPrivate.data.shared = new Shared<string>(new string(*static_cast<string *>(val)));
 				mPrivate.isShared = 1;
 				break;
-			
+
 			case DV_VECTOR :
 				mPrivate.data.shared = new Shared<Vector3>(new Vector3(*static_cast<Vector3 *>(val)));
 				mPrivate.isShared = 1;
 				break;
-				
+
 			default :
-				throw runtime_error("DVariant::typeToString() - invalid type");
+				throw runtime_error("DVariant::DVariant() - invalid type");
 		}
-		
-		
+
+
 		mPrivate.type = t;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(Type t, const std::string& txtval) {
 		mPrivate.isShared = false;
-		
+
 		switch (t) {
 			case DV_BOOL :
 				mPrivate.data.dbool = StringToBool(txtval);
 				break;
-				
+
 			case DV_FLOAT :
 				mPrivate.data.dfloat = StringToFloat(txtval);
 				break;
-				
+
 			case DV_INT :
 				mPrivate.data.dint = StringToInt(txtval);
 				break;
-				
+
 			case DV_UINT :
 				mPrivate.data.duint = StringToUInt(txtval);
 				break;
@@ -98,68 +98,68 @@ namespace Opde {
 				mPrivate.data.shared = new Shared<string>(new string(txtval));
 				mPrivate.isShared = 1;
 				break;
-			
+
 			case DV_VECTOR :
 				mPrivate.data.shared = new Shared<Vector3>(new Vector3(StringToVector(txtval)));
 				mPrivate.isShared = 1;
 				break;
-			
+
 			default :
-				throw runtime_error("DVariant::typeToString() - invalid type");
+				throw runtime_error("DVariant::DVariant() - invalid type");
 		}
-		
+
 		mPrivate.type = t;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(const DVariant& b) {
 		if (b.mPrivate.isShared)
 			b.mPrivate.data.shared->addRef();
-		
+
 		mPrivate = b.mPrivate;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(bool val) {
 		mPrivate.type = DV_BOOL;
 		mPrivate.data.dbool = val;
 		mPrivate.isShared = false;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(float val) {
 		mPrivate.type = DV_FLOAT;
 		mPrivate.data.dfloat = val;
 		mPrivate.isShared = false;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(int val) {
 		mPrivate.type = DV_INT;
 		mPrivate.data.dint = val;
 		mPrivate.isShared = false;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(uint val) {
 		mPrivate.type = DV_UINT;
 		mPrivate.data.duint = val;
 		mPrivate.isShared = false;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(const char* text, int length) {
 		mPrivate.type = DV_STRING;
-		
+
 		if (length > 0) { // if length is present... (I could probably see what the default length param is, but it could differ)
 			mPrivate.data.shared = new Shared<string>(new string(text, length));
 		} else {
 			mPrivate.data.shared = new Shared<string>(new string(text));
 		}
-		
+
 		mPrivate.isShared = true;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(const std::string& text) {
 		mPrivate.type = DV_STRING;
@@ -173,20 +173,20 @@ namespace Opde {
 		mPrivate.data.shared = new Shared<Vector3>(new Vector3(vec));
 		mPrivate.isShared = true;
 	}
-	
+
 	//------------------------------------
 	DVariant::DVariant(float x, float y, float z) {
 		mPrivate.type = DV_VECTOR;
 		mPrivate.data.shared = new Shared<Vector3>(new Vector3(x, y, z));
 		mPrivate.isShared = true;
 	}
-	
+
 	//------------------------------------
 	DVariant::~DVariant() {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
 	}
-	
+
 	//------------------------------------
 	const char* DVariant::typeToString() const {
 		switch (mPrivate.type) {
@@ -203,15 +203,15 @@ namespace Opde {
 			case DV_VECTOR :
 				return "Vector";
 			default:
-				throw runtime_error("DVariant::typeToString() - invalid type");
+                return "Invalid"; // Be gentle. Do not throw
 		}
 	}
-	
+
 	//------------------------------------
 	DVariant::operator std::string() const {
 		std::ostringstream o;
 		Vector3* vecd;
-		
+
 		switch (mPrivate.type) {
 			case DV_BOOL :
 				return mPrivate.data.dbool ? "true" : "false";
@@ -231,19 +231,19 @@ namespace Opde {
 				o << vecd->x << ", " << vecd->y << ", " << vecd->z;
 				return o.str();
 			default:
-				throw runtime_error("DVariant::typeToString() - invalid type");
+				throw runtime_error("DVariant::string() - invalid type");
 		}
 	}
-	
+
 	//------------------------------------
 	string DVariant::toString() const {
 		return operator string();
 	}
-	
+
 	//------------------------------------
 	int DVariant::toInt() const {
 		std::stringstream ssStream;
-		
+
 		switch (mPrivate.type) {
 			case DV_BOOL :
 				return mPrivate.data.dbool ? 1 : 0;
@@ -261,7 +261,7 @@ namespace Opde {
 				throw runtime_error("DVariant::toInt() - invalid type specified");
 		}
 	}
-	
+
 	//------------------------------------
 	uint DVariant::toUInt() const {
 		switch (mPrivate.type) {
@@ -278,10 +278,10 @@ namespace Opde {
 			case DV_VECTOR :
 				throw runtime_error("DVariant::toUInt() - vector cannot be cast to int");
 			default:
-				throw runtime_error("DVariant::toInt() - invalid type specified");
+				throw runtime_error("DVariant::toUInt() - invalid type specified");
 		}
 	}
-	
+
 	//------------------------------------
 	float DVariant::toFloat() const {
 		std::stringstream ssStream;
@@ -300,10 +300,10 @@ namespace Opde {
 			case DV_VECTOR :
 				throw runtime_error("DVariant::toFloat() - vector cannot be cast to int");
 			default:
-				throw runtime_error("DVariant::toInt() - invalid type specified");
+				throw runtime_error("DVariant::toFloat() - invalid type specified");
 		}
 	}
-	
+
 	//------------------------------------
 	bool DVariant::toBool() const {
 		switch (mPrivate.type) {
@@ -320,10 +320,10 @@ namespace Opde {
 			case DV_VECTOR :
 				throw runtime_error("DVariant::toBool() - vector cannot be cast to bool");
 			default:
-				throw runtime_error("DVariant::toInt() - invalid type specified");
+				throw runtime_error("DVariant::toBool() - invalid type specified");
 		}
 	}
-	
+
 	//------------------------------------
 	Vector3 DVariant::toVector() const {
 		if (mPrivate.type == DV_VECTOR) {
@@ -335,124 +335,124 @@ namespace Opde {
 			throw runtime_error("DVariant::toVector - Uncompatible source type");
 		}
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(const DVariant& b) {
 		if (b.mPrivate.isShared)
 			b.mPrivate.data.shared->addRef();
-		
+
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate = b.mPrivate;
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(bool b) {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.isShared = false;
 		mPrivate.data.dbool = b;
 		mPrivate.type = DV_BOOL;
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(int i)  {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.type = DV_INT;
 		mPrivate.isShared = false;
 		mPrivate.data.dint = i;
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(uint u)  {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.type = DV_UINT;
 		mPrivate.isShared = false;
 		mPrivate.data.duint = u;
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(float f)  {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.type = DV_FLOAT;
 		mPrivate.isShared = false;
 		mPrivate.data.dfloat = f;
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(char* s) {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.isShared = true;
 		mPrivate.type = DV_STRING;
 		mPrivate.data.shared = new Shared<string>(new string(s));
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(std::string s) {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.isShared = true;
 		mPrivate.type = DV_STRING;
 		mPrivate.data.shared = new Shared<string>(new string(s));
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	const DVariant& DVariant::operator =(Vector3 v) {
 		if (mPrivate.isShared)
 			mPrivate.data.shared->release();
-		
+
 		mPrivate.isShared = true;
 		mPrivate.type = DV_VECTOR;
 		mPrivate.data.shared = new Shared<Vector3>(new Vector3(v));
-		
+
 		return *this;
 	}
-	
+
 	//------------------------------------
 	DVariant::Type DVariant::type() const {
 		return mPrivate.type;
 	}
-	
+
 	//------------------------------------
 	// Returns a reference to a shared date, dynamically cast to the requested type, if possible
 	template <typename T> T& DVariant::shared_cast() const {
 		if (mPrivate.isShared == false)
 			throw(runtime_error("Invalid shared_cast - cast on non-shared data"));
-		
+
 		Shared<T> *ct = dynamic_cast<Shared<T>*>(mPrivate.data.shared);
-		
+
 		if (ct != NULL)
 			return *(ct->data);
 		else
 			throw(runtime_error("Invalid shared_cast - cast on different type or NULL"));
-		
+
 	}
-	
+
 	//------------------------------------
 	bool DVariant::compare(const DVariant &b) const {
 		if (b.type() == type()) {
@@ -466,13 +466,13 @@ namespace Opde {
 				case DV_UINT :
 					return b.mPrivate.data.duint == mPrivate.data.duint;
 				case DV_STRING:
-					return 
-						b.shared_cast<string>() == 
+					return
+						b.shared_cast<string>() ==
 						shared_cast<string>();
-				case DV_VECTOR: 
+				case DV_VECTOR:
 					return 	b.shared_cast<Vector3>() ==
 						shared_cast<Vector3>();
-						
+
 				default:
 					throw(runtime_error("DVariant: Invalid compare type"));
 			}
@@ -481,46 +481,46 @@ namespace Opde {
 			switch (mPrivate.type) {
 				case DV_BOOL : // any nonzero value will be considered true
 					return mPrivate.data.dbool == b.toBool();
-				
+
 				case DV_FLOAT :
 					return b.toFloat() == mPrivate.data.dfloat;
-					
+
 				case DV_UINT :
 					return toUInt() == b.toUInt();
 				case DV_INT  :
 					return toInt() == b.toInt();
-					
-				case DV_STRING : 
+
+				case DV_STRING :
 					return shared_cast<string>() == b.toString();
-				
-				case DV_VECTOR : 
+
+				case DV_VECTOR :
 					return shared_cast<Vector3>() == b.toVector();
-				
+
 				default :
 					throw runtime_error("DVariant::typeToString() - invalid type");
 			}
 		}
-		
+
 		return false;
 	};
-	
+
 	//------------------------------------
 	Vector3 DVariant::StringToVector(const std::string& str) {
 		string src(str);
-		
+
 		float vec[3];
 		vec[0] = vec[1] = vec[2] = 0;
 		int cnt = 0;
-		
+
 		while (cnt <= 2) {
 			// parse the string
 			size_t comma_pos = src.find(',');
-			
+
 			// last value does not end with comma
 			if (cnt == 2) {
 					std::stringstream ssStream(src);
 					ssStream >> vec[cnt];
-					
+
 					if (!ssStream)
 						throw runtime_error(string("DVariant::StringToVector - Parse error for ") + str);
 			} else {
@@ -528,85 +528,85 @@ namespace Opde {
 					std::stringstream ssStream(src.substr(0, comma_pos));
 					ssStream >> vec[cnt];
 					ssStream.clear();
-					
+
 					if (!ssStream)
 						throw runtime_error(string("DVariant::StringToVector - Parse error for ") + str);
-					
+
 					src = src.substr(comma_pos + 1, src.length() - (comma_pos + 1));
 				} else {
 					throw runtime_error(string("DVariant::StringToVector - Parse error for ") + str);
 					// return Vector3(0,0,0);
 				}
 			}
-			
+
 			cnt++;
 		}
-		
+
 		// make a vector3 out of the array, and return
 		return Vector3(vec[0], vec[1], vec[2]);
 	}
-	
+
 	//------------------------------------
 	int DVariant::StringToInt(const std::string& str) {
 		std::stringstream ssStream;
 		int iReturn;
-		
+
 		if (str.substr(0,2)=="0x") { // hexadecimal
 			ssStream >> hex;
 			ssStream << str.substr(2);
     			ssStream >> iReturn;
-		
+
 			if (!ssStream)
 				throw runtime_error(string("DVariant::StringToInt - Parse error for ") + str);
 		} else {
 			ssStream << str;
     			ssStream >> iReturn;
-		
+
 			if (!ssStream)
 				throw runtime_error(string("DVariant::StringToInt - Parse error for ") + str);
 		}
-		
+
 		return iReturn;
 	}
-	
+
 	//------------------------------------
 	uint DVariant::StringToUInt(const std::string& str) {
 		std::stringstream ssStream;
 		uint iReturn;
-		
+
 		if (str.substr(0,2)=="0x") { // hexadecimal
 			ssStream >> hex;
 			ssStream << str.substr(2);
     			ssStream >> iReturn;
-		
+
 			if (!ssStream)
 				throw runtime_error(string("DVariant::StringToInt - Parse error for ") + str);
 		} else {
 			ssStream << str;
     			ssStream >> iReturn;
-		
+
 			if (!ssStream)
 				throw runtime_error(string("DVariant::StringToInt - Parse error for ") + str);
 		}
-		
+
 		return iReturn;
 	}
-	
+
 	//------------------------------------
 	bool DVariant::StringToBool(const std::string& str) {
 		string s(str);
-		
+
 		// HMM: VC does not like this.
 		// transform(s.begin(), s.end(), s.begin(), (int(*)(int)) std::tolower);
-		
+
 		if ((s=="true") || (s=="1") || (s=="yes")) {
 			return true;
 		} else if ((s=="false") || (s=="0") || (s=="no")) {
 			return false;
-		} else 
+		} else
 			throw runtime_error(string("DVariant::StringToBool - Parse error for ") + str);
 	}
-	
+
 	//------------------------------------
 	float DVariant::StringToFloat(const std::string& str) {
 		std::stringstream ssStream;
@@ -614,11 +614,11 @@ namespace Opde {
 
 		ssStream << str;
     		ssStream >> fReturn;
-		 
+
 		if (!ssStream)
 			throw runtime_error(string("DVariant::StringToFloat - Parse error for ") + str);
-		
+
 		return fReturn;
 	}
-	
+
 }
