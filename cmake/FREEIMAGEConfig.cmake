0317d04e10695c409202d5e4a3a65df7d3aff1d2
Filip Volejnik <f.volejnik@centrum.cz>
2007-11-27 11:17:50 +0000

    Finder for FreeImage, now for windows as well (blindly written code, but should work)

--- cmake/FREEIMAGEConfig.cmake
+++ cmake/FREEIMAGEConfig.cmake
@@ -11,7 +11,13 @@ MARK_AS_ADVANCED(FREEIMAGE_FOUND)
 MARK_AS_ADVANCED(FREEIMAGE_LIBRARIES)
 MARK_AS_ADVANCED(FREEIMAGE_INCLUDE_DIR)
 
+# For win32 builds. This can be filled with
+SET(FREEIMAGE_PATH "" CACHE STRING "For Win32 lib. search. Can be filled to the path of the unzipped FreeImageXXXXWin32.zip (Including FreeImage Directory)")
+
 IF (UNIX)
+    # Hide the FREEIMAGE_PATH, not needed under unix systems
+    MARK_AS_ADVANCED(FREEIMAGE_PATH)
+    
     # Let's look for lib freeimage
     # There is no pkg-config for freeimage, so we have to search by hand
     SET(FREEIMAGE_INC_SEARCH_PATH
@@ -50,5 +56,32 @@ IF (UNIX)
 ENDIF(UNIX)
 
 IF(WIN32)
-    SET(FREEIMAGE_FOUND 0)
+    SET(FREEIMAGE_INC_SEARCH_PATH
+        $ENV{FREEIMAGE_PATH}/Dist
+	${FREEIMAGE_PATH}/Dist
+    )
+
+    SET(FREEIMAGE_LIB_SEARCH_PATH
+        $ENV{FREEIMAGE_PATH}/Dist
+	${FREEIMAGE_PATH}/Dist
+    )
+
+    # That's it. The library name without the lib and .so
+    SET(FREEIMAGE_LIBNAMES freeimage)
+    
+    # The search begins
+    FIND_PATH(FREEIMAGE_INCLUDE_DIR FreeImage.h ${FREEIMAGE_INC_SEARCH_PATH})
+
+    IF(NOT FREEIMAGE_INCLUDE_DIR)
+        MESSAGE("Warning: FreeImage.h not found under ${FREEIMAGE_INC_SEARCH_PATH}")
+        SET(FREEIMAGE_FOUND 0)
+    ENDIF(NOT FREEIMAGE_INCLUDE_DIR)
+    
+    # Headers up ok, now the lib
+    FIND_LIBRARY(FREEIMAGE_LIBRARIES NAME ${FREEIMAGE_LIBNAMES} PATHS ${FREEIMAGE_SEARCH_PATH})
+
+    IF(NOT FREEIMAGE_LIBRARIES)
+        MESSAGE("Warning: FreeImage library not found under ${FREEIMAGE_LIB_SEARCH_PATH}")
+        SET(FREEIMAGE_FOUND 0)
+    ENDIF(NOT FREEIMAGE_LIBRARIES)
 ENDIF(WIN32)
