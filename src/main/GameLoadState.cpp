a7b5102772321a5c0ca7582fe1446d518baca55d
Ali Pouladi <dude_170@yahoo.com>
2007-12-05 23:36:08 +0000

    Added the font name to the loading message, good for debugging.

--- src/main/GameLoadState.cpp
+++ src/main/GameLoadState.cpp
@@ -17,6 +17,10 @@
  *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ *
+ *
+ *    $Id$
+ *
  *****************************************************************************/
 
 #include "OIS.h"
@@ -247,7 +251,7 @@ namespace Opde {
 			textArea->setDimensions(640, 45);
 
 			// textArea->setCaption(*it);
-			textArea->setCaption("Keepers would like this font. LOADING!");
+			textArea->setCaption("Keepers would like this font: " + *it+ ", LOADING!");
 
             height = 16;
 			textArea->setCharHeight(height); // Todo: size
