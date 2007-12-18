fab4d4bd7cd6ab1ee55076e1519fdf66008be9b7
Filip Volejnik <f.volejnik@centrum.cz>
2007-12-18 20:53:00 +0000

    Adding relation binder stub

---
+++ proto/python/RelationBinder.h
@@ -0,0 +1,64 @@
+/******************************************************************************
+ *
+ *    This file is part of openDarkEngine project
+ *    Copyright (C) 2005-2006 openDarkEngine team
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *	   $Id$
+ *
+ *****************************************************************************/
+
+#ifndef __RELATIONBINDER_H
+#define __RELATIONBINDER_H
+
+#include  "DTypeDef.h"
+#include  "BinaryService.h"
+#include  "LinkService.h"
+#include  "Relation.h"
+
+namespace Opde {
+
+	namespace Python {
+
+		/// Link service python binder
+		class RelationBinder : public shared_ptr_binder<RelationPtr> {
+			public:
+				// --- Python type related methods ---
+				static PyObject* getattr(PyObject *self, char *name);
+
+				/// creates a python object representation of the relation
+				static PyObject* create(RelationPtr relation);
+
+				// --- Methods ---
+				static PyObject* getID(PyObject* self, PyObject* args);
+				static PyObject* getName(PyObject* self, PyObject* args);
+				static PyObject* remove(PyObject* self, PyObject* args);
+				
+			protected:
+				/// Static type definition for LinkService
+				static PyTypeObject msType;
+
+				/// Name of the python type
+				static char* msName;
+
+				/// Method list
+				static PyMethodDef msMethods[];
+		};
+	}
+}
+
+#endif
