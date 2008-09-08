11f2d8e89327ad8b82c11fb0a9f47306d92e71ce
Filip Volejnik <f.volejnik@centrum.cz>
2008-09-08 18:20:33 +0000

    just to be sure - clear the inheritor link (leak fix possibly)

--- src/services/property/PropertyGroup.cpp
+++ src/services/property/PropertyGroup.cpp
@@ -59,6 +59,8 @@ namespace Opde {
 
 		if (! mInheritor.isNull())
 			mInheritor->unregisterListener(mInheritorListenerID);
+			
+		mInheritor = NULL;
 	}
 	
 	// --------------------------------------------------------------------------
