7bec5324c51dc6119990eb5a6f5fbe5099516179
Ali Pouladi <dude_170@yahoo.com>
2008-12-23 21:58:01 +0000

    Added ODE to CMake. Filip could you please verify the Linux setup?

---
+++ cmake/ODEConfig.cmake
@@ -0,0 +1,48 @@
+#
+# Try to find ODE headers on both Windows and Linux systems
+# 
+# The following values will get defined:
+#	ODE_FOUND - True if ODE was found
+#	ODE_INCLUDE_DIR - Include headers for ODE usage
+#
+# $Id$
+
+SET(ODE_FOUND)
+MARK_AS_ADVANCED(ODE_FOUND)
+MARK_AS_ADVANCED(ODE_INCLUDE_DIR)
+
+# For win32 builds. This can be filled by hand. Standard path is dependencies directory next to opde binary dir (out of source build target directory)
+SET(ODE_PATH "${OPDE_BINARY_DIR}/../dependencies/ode/" CACHE STRING "For Win32 include directories and the installer. Can be filled to the path of the ODE")
+
+IF (UNIX)
+    # Let's look for ODE
+    SET(ODEE_INC_SEARCH_PATH
+        /usr/include/
+        /usr/local/include/
+     )
+
+    # The search begins
+    FIND_PATH(ODE_INCLUDE_DIR ode.h ${ODE_INC_SEARCH_PATH})
+
+    IF(NOT ODE_INCLUDE_DIR)
+        MESSAGE("Warning: ode.h not found under ${ODE_INC_SEARCH_PATH}")
+        SET(ODE_FOUND 0)
+    ENDIF(NOT ODE_INCLUDE_DIR)
+    
+ENDIF(UNIX)
+
+IF(WIN32)
+    SET(ODE_INC_SEARCH_PATH
+        $ENV{ODE_PATH}/include/ode
+	${ODE_PATH}/include/ode
+    )
+    
+    # The search begins
+    FIND_PATH(ODE_INCLUDE_DIR ode.h ${ODE_INC_SEARCH_PATH})
+
+    IF(NOT ODE_INCLUDE_DIR)
+        MESSAGE("Warning: ode.h not found under ${ODE_INC_SEARCH_PATH}")
+        SET(ODE_FOUND 0)
+    ENDIF(NOT ODE_INCLUDE_DIR)
+    
+ENDIF(WIN32)
