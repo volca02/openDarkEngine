57a1c05b3f44cc108947dc20472618a83c54ee1a
Filip Volejnik <f.volejnik@centrum.cz>
2009-02-06 06:57:28 +0000

    Copyright year fix (I always forget about this one!)

--- src/tools/physver.cpp
+++ src/tools/physver.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  *    This file is part of openDarkEngine project
- *    Copyright (C) 2005-2007 openDarkEngine team
+ *    Copyright (C) 2009 openDarkEngine team
  *
  *    This program is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
