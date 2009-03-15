1e50538b5ae8092d42fbbaaa9290534cc5cbbee3
Ali Pouladi <dude_170@yahoo.com>
2009-03-15 08:52:30 +0000

    Fixed the keywords.

--- src/tools/physver.cpp
+++ src/tools/physver.cpp
@@ -17,7 +17,7 @@
  *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *	  $Id: chunk.cpp 1116 2009-01-18 21:34:20Z volca $
+ *	  $Id$
  *
  *****************************************************************************/
 
