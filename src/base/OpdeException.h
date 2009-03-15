1063cfe16903152096d0c46cfc251e4bbddd7530
Ali Pouladi <dude_170@yahoo.com>
2009-03-15 08:28:18 +0000

    Fixed copyright dates.

--- src/base/OpdeException.h
+++ src/base/OpdeException.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  *    This file is part of openDarkEngine project
- *    Copyright (C) 2005-2006 openDarkEngine team
+ *    Copyright (C) 2005-2009 openDarkEngine team
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free Software
