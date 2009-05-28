8d3acca0e90ca5506a0cb52520bfff4c213e2a29
Ali Pouladi <dude_170@yahoo.com>
2009-05-28 00:21:41 +0000

    Adding SVN keywords.

--- src/bindings/RenderedImageBinder.h
+++ src/bindings/RenderedImageBinder.h
@@ -18,7 +18,7 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *
- *	   $Id: DrawSheetBinder.h 1193 2009-05-18 12:59:46Z volca $
+ *	   $Id$
  *
  *****************************************************************************/
 
