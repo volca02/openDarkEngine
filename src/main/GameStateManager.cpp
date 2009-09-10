d18729421bb676d90842c76676ff24d40ea10638
Filip Volejnik <f.volejnik@centrum.cz>
2009-09-10 19:23:46 +0000

    full mask service initialization now in Opde::Root

--- src/main/GameStateManager.cpp
+++ src/main/GameStateManager.cpp
@@ -232,9 +232,7 @@ namespace Opde {
 
 		LOG_INFO("GameStateManager: Finishing bootstrap");
 
-        // Temporary create to allow Room service creation and testing
-        ServiceManager::getSingleton().createByMask(SERVICE_ALL);
-        mRoot->bootstrapFinished();
+		mRoot->bootstrapFinished();
 
 		ps->bootstrapFinished();
 
