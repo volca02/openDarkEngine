a3b027d2b7e495603d954954f7e348ddb316a3ba
Ali Pouladi <dude_170@yahoo.com>
2009-09-17 11:49:41 +0000

    Fixed keyword substitution.

--- src/services/gui/ConsoleGUI.cpp
+++ src/services/gui/ConsoleGUI.cpp
@@ -17,7 +17,7 @@
  *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *	  $Id: ConsoleFrontend.cpp 1163 2009-03-15 08:48:57Z patryn $
+ *	  $Id$
  *
  *****************************************************************************/
 
